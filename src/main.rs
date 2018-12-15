6005ae6bc0184988819bf4116ec475e52acbca1b
Christopher Karani <chrisbkarani@gmail.com>
2018-12-15 21:18:37 +0300

    made a working terminal GUI with control flow

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,28 @@ fn main() {
 
     siv.add_global_callback('q', |s| s.quit());
 
-    siv.add_layer(Dialog::text("Hello Christopher"));
+    siv.add_layer(Dialog::text("Hello Christopher")
+        .title("Hello World")
+        .button("Quit", |s|show_next(s)));
+
+
 
     siv.run()
+}
+
+fn show_next(s: &mut Cursive){
+    s.pop_layer();
+    s.add_layer(Dialog::text("Are you Gay? 😏")
+        .title("Question 1")
+        .button("Yes", |s| show_answer(s, "I Knew It Lmao 😉"))
+        .button("No!", |s| show_answer(s, "Oh I meant as in like happy,👀 are you happy? 😅"))
+        .button("Uh?", |s| s.add_layer(Dialog::text("try again"))));
+
+}
+
+fn show_answer(s: &mut Cursive, msg: &str) {
+    s.pop_layer();
+    s.add_layer(Dialog::text(msg)
+                    .title("Results")
+                    .button("Finish", |s| s.quit()))
 }
